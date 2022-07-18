ed40e4f6626b20a92cc6b545dfc7fb3d043e72cb
Okiki <okikio.dev@gmail.com>
2022-07-18 03:15:08 +0000

    fix: codemirror working in Astro

--- src/components/codemirror.ts
+++ src/components/codemirror.ts
@@ -1,23 +1,14 @@
 import { EditorView, basicSetup as BASIC_SETUP } from "codemirror";
-// import { keymap } from "@codemirror/view";
-// import { indentWithTab } from "@codemirror/commands";
+import { keymap } from "@codemirror/view";
 
-// import { javascript } from "@codemirror/lang-javascript";
-import { json, jsonParseLinter } from "@codemirror/lang-json";
+import { indentWithTab } from "@codemirror/commands";
+import { json, jsonLanguage, jsonParseLinter } from "@codemirror/lang-json";
 
-// import { BASIC_SETUP, EditorView, EditorState } from "./editor-basic-setup";
-// import { THEME, HIGHTLIGHT_STYLE } from "./editor-theme";
-// import type {
-//   HighlightStyle
-// } from "@codemirror/language";
+import { foldGutter, foldKeymap, syntaxHighlighting } from "@codemirror/language"
 
-// import type { Extension } from "@codemirror/state";
+import { THEME, HIGHTLIGHT_STYLE } from "./editor-theme";
+import type { Extension } from "@codemirror/state";
 
-// import {foldGutter, foldKeymap} from "@codemirror/language"
-  // foldGutter({
-  //     openText: "expand_more",
-  //     closedText: "chevron_right"
-  // }),
 
 const initalText = `\
 {
@@ -32,20 +23,23 @@ const initalText = `\
 }
 `;
 // Extension
-export default (parentEl: HTMLElement, extentions?: any[], doc = initalText) => {
+export default (parentEl: HTMLElement, ext: Extension[] = [], doc = initalText) => {
   return new EditorView({
     doc,
     extensions: [
       BASIC_SETUP,
 
-      // THEME,
-      // HIGHTLIGHT_STYLE,
+      THEME,
+      syntaxHighlighting(HIGHTLIGHT_STYLE),
 
-      // keymap.of([indentWithTab]),
-      // json(),
-      // jsonParseLinter()
-    ].concat(extentions),
+      // foldGutter({
+      //   openText: "expand_more",
+      //   closedText: "chevron_right"
+      // }),
+
+      keymap.of([indentWithTab]),
+      json()
+    ].concat(ext),
     parent: parentEl
   });
-  //  as Extension[]
 };
--- src/open-rpc-client-js/index.ts
+++ src/open-rpc-client-js/index.ts
@@ -1,20 +1,12 @@
 import RequestManager from "./RequestManager";
-import EventEmitterTransport from "./transports/EventEmitterTransport";
-// import HTTPTransport from "./transports/HTTPTransport";
-// import WebSocketTransport from "./transports/WebSocketTransport";
-// import PostMessageWindowTransport from "./transports/PostMessageWindowTransport";
-// import PostMessageIframeTransport from "./transports/PostMessageIframeTransport";
 import { JSONRPCError } from "./Error";
 import Client from "./Client";
+import PostMessageWorkerTransport from "./transports/PostMessageWorkerTransport";
 
 export default Client;
 export {
   Client,
   RequestManager,
-  // HTTPTransport,
-  EventEmitterTransport,
-  // WebSocketTransport,
-  JSONRPCError,
-  // PostMessageWindowTransport,
-  // PostMessageIframeTransport,
+  PostMessageWorkerTransport,
+  JSONRPCError
 };
