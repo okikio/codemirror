4eabaa2d111648539ac7ae3125672f9fa2173ac7
Okiki <okikio.dev@gmail.com>
2022-07-22 01:40:15 +0000

    html...

--- src/scripts/codemirror.ts
+++ src/scripts/codemirror.ts
@@ -3,6 +3,7 @@ import { keymap } from "@codemirror/view";
 
 import { indentWithTab } from "@codemirror/commands";
 import { javascript } from "@codemirror/lang-javascript";
+import { html } from "@codemirror/lang-html";
 
 import { foldGutter, foldKeymap, syntaxHighlighting } from "@codemirror/language";
 import { linter, lintGutter, lintKeymap } from "@codemirror/lint";
@@ -12,10 +13,18 @@ import { THEME, HIGHTLIGHT_STYLE } from "../components/editor-theme";
 import type { Extension } from "@codemirror/state";
 
 const initalText = `\
-export * from "@okikio/animate";
-export const el = () => {
-  return (<h1>Hello</h1>)
-};
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 `;
 
 export default (parentEl: HTMLElement, ext: Extension[] = [], doc = initalText) => {
@@ -32,10 +41,12 @@ export default (parentEl: HTMLElement, ext: Extension[] = [], doc = initalText) 
       keymap.of(completionKeymap),
       keymap.of(foldKeymap),
 
-      javascript({
-        jsx: true,
-        typescript: true
-      }),
+      // javascript({
+      //   jsx: true,
+      //   typescript: true
+      // }),
+
+      html(),
 
       // foldGutter({
       //   openText: "expand_more",
